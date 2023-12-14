ae5fed23f1638aa85ae22909ec6ad771a1ca219c
Saran Pariyar <spariyar05@gmail.com>
2023-12-14 09:44:28 +0545

    Made UI, install next-auth, mongoose

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
-// import UserInfo from "@/components/UserInfo";
+import UserInfo from "@/components/UserInfo";
 
 export default function Home() {
   return (
     <div className="grid place-items-center h-screen -mt-24">
-      {/* <UserInfo /> */}
+      <UserInfo />
       Hello
     </div>
   );
